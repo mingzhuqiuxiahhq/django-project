799e660d2568e2fc186d28372ff9727c4d83ee29
mingzhuqiuxiahhq <hangqinghe@gmail.com>
2022-01-28 19:56:40 -0600

    amend html file

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from django.utils import timezone
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
